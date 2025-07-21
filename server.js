039adc3683689859183b7ed32ce2600ec600ff69
Kaligetsagency <kaligetsagency@gmail.com>
2025-07-22 01:33:57 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({ origin: "https://index.html" }));  
 app.use(express.json());
 
 // Deriv API credentials
